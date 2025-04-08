0d668fde019147cf7ecd6182ca6e157bdde604fe
Soham Nehra <sohamstark@gmail.com>
2025-04-09 02:14:36 +0530

    end of 08-04-2025

--- train_gpt2.py
+++ train_gpt2.py
@@ -16,4 +16,12 @@ class GPT(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config=config
-    
+
+        self.transformer = nn.ModuleDict(dict(
+            wte=nn.Embedding(config.vocab_size, config.n_embd),
+            wpe=nn.Embedding(config.block_size, config.n_embd),
+            drop=nn.Dropout(0.1),
+            h=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f=nn.LayerNorm(config.n_embd),
+        ))
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
